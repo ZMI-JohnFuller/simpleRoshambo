b6192a1ddbaf478a8c46c9321bf64ff14f3549db
jfuller-bnet <john.fuller.bnet@gmail.com>
2017-02-22 13:22:49 -0500

    Updated main code
    Updated main code to reflect current main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python
+global userInput
+
+def getInput(userInput):
+    userInput == input('Please enter your choice:\nRock, Paper or Scissors? (Type "Quit" to exit)\n')
+    validateInput(userInput)
+
+def validateInput(userInput):
+    print ('validating input...')
+    if userInput == '':
+        return print('Invalid input')
+    if userInput == 'Quit':
+        exit()
+    else:
+        print('You chose "' + userInput + '" ')
+
+def inputHandler(userInput):
+    getInput(userInput)
+
+def main():
+    while True:
+        inputHandler(userInput)
+main()
