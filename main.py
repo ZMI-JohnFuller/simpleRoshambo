bdbfa0600c485c431ef06902aff4f4dbf84d14cd
jfuller-bnet <john.fuller.bnet@gmail.com>
2017-02-22 13:43:01 -0500

    MIKE HELPED MEEE

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python
-global userInput
+import sys
 
-def getInput(userInput):
-    userInput == input('Please enter your choice:\nRock, Paper or Scissors? (Type "Quit" to exit)\n')
+def getInput():
+    userInput = input('Please enter your choice:\nRock, Paper or Scissors? (Type "Quit" to exit)\n')
+    print(userInput)
     validateInput(userInput)
+    print(userInput)
 
 def validateInput(userInput):
     print ('validating input...')
-    if userInput == '':
-        return print('Invalid input')
-    if userInput == 'Quit':
-        exit()
+    if userInput:
+        if userInput.lower() not in ['rock', 'paper', 'scissors']:
+                print('Nope!')
+        elif userInput is '':
+            print('Invalid input')
+        elif userInput.lower() in 'quit':
+            sys.exit(2)
+        elif userInput.lower() in ['rock','r']:
+            print ('you chose rock')
     else:
-        print('You chose "' + userInput + '" ')
-
-def inputHandler(userInput):
-    getInput(userInput)
-
+        print('stop messing up')
 def main():
     while True:
-        inputHandler(userInput)
+        getInput()
+
 main()
