e34a8ef9af4d91a821da0362426df26ef4bfafc1
jfuller-bnet <john.fuller.bnet@gmail.com>
2017-02-23 13:58:08 -0500

    Greg helped me!
    Greg is pretty cool

--- main.py
+++ main.py
@@ -1,37 +1,25 @@
 #!/usr/bin/env python
-
 import sys
 import random
 
 while True:
-    randChoice = random.randint(0,2)
-    userChoice = input('Enter 1 for rock, 2 for paper and 3 for scissors. Enter "q" to quit\n')
+    randChoice = str(random.randint(0,2))
+    userChoice = raw_input('Enter 0 for rock, 1 for paper and 2 for scissors. Enter "q" to quit\n')
     print(userChoice)
     print(randChoice)
-    if userChoice in ['1','2','3','q']:
-        if userChoice == 'q':
-            sys.exit('Goodbye!')
-        if userChoice == '1':
-            if randChoice == 0:
-                print('Tie')
-            if randChoice == 1:
-                print('Lose')
-            if randChoice == 2:
-                print('Win')
-        if userChoice == '2':
-            if randChoice == 0:
-                print('Win!')
-            if randChoice == 1:
-                print('Tie')
-            if randChoice == 2:
-                print('Lose')
-        if userChoice == '3':
-            if randChoice == 0:
-                print('Lose')
-            if randChoice == 1:
-                print('Win!')
-            if randChoice == 2:
-                print('Tie')
 
+    if str(userChoice) in ['1','2','3','q']:
+        if str(userchoice) == 'q':
+            sys.exit('Goodbye!')
+        if str(userChoice) == randChoice:
+            print "Tie"
+        elif userChoice == "0" and randChoice == "2":
+            print "You Win"
+        elif userChoice == "1" and randChoice == "0":
+            print "You Win"
+        elif userChoice == "2" and randChoice == "1":
+            print "You Win"
+        else:
+            print "You Lose"
     else:
         print('invalid input')
