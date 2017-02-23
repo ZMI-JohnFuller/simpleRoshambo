744865e478408e6879d23b53c2b46c8df21222fe
jfuller-bnet <john.fuller.bnet@gmail.com>
2017-02-23 16:32:48 -0500

    FINAL VERSION!
    Final version thank you to bryan john mike and greg!

--- main.py
+++ main.py
@@ -3,23 +3,35 @@ import sys
 import random
 
 while True:
-    randChoice = str(random.randint(0,2))
-    userChoice = raw_input('Enter 0 for rock, 1 for paper and 2 for scissors. Enter "q" to quit\n')
-    print(userChoice)
-    print(randChoice)
-
+    userChoice = input('Enter 1 for Rock, 2 for Paper and 3 For Scissors. To quit, enter "Q"\n')
     if str(userChoice) in ['1','2','3','q']:
-        if str(userchoice) == 'q':
+        if str(userChoice) == 'q':
             sys.exit('Goodbye!')
-        if str(userChoice) == randChoice:
-            print "Tie"
-        elif userChoice == "0" and randChoice == "2":
-            print "You Win"
-        elif userChoice == "1" and randChoice == "0":
-            print "You Win"
-        elif userChoice == "2" and randChoice == "1":
-            print "You Win"
         else:
-            print "You Lose"
+            #AI conditionals
+            randChoice = str(random.randint(1, 3))
+            print (randChoice)
+            if randChoice == "1":
+                print("They chose Rock...")
+            if randChoice == "2":
+                print("They chose Paper...")
+            if randChoice == "3":
+                print("They chose Scissors...")
+
+            #user conditionals
+
+            if userChoice == "1":
+                print("You chose Rock...")
+            if userChoice == "2":
+                print("You chose Paper...")
+            if userChoice == "3":
+                print("You chose Scissors...")
+
+            if str(userChoice) == randChoice:
+                print ("Tie")
+            elif userChoice == "1" and randChoice == "3" or userChoice == "2" and randChoice == "1" or userChoice =="3" and randChoice =="2":
+                print("You win!")
+            else:
+                print('You lose.')
     else:
-        print('invalid input')
+        print('Invalid input.\n')
