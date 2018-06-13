2f6f44e35c9edc9bafb4ff8f77e1045602a42243
Bryan Bailey <bryan.bailey@bethesda.net>
2018-06-12 21:02:44 -0400

    Refactor
    Refactored the initial code. Setting up to implement additional games by encapsulating the original code in its own method. Going to add Hangman to this when I have the time

--- main.py
+++ main.py
@@ -2,36 +2,78 @@
 import sys
 import random
 
-while True:
-    userChoice = input('Enter 1 for Rock, 2 for Paper and 3 For Scissors. To quit, enter "Q"\n')
-    if str(userChoice) in ['1','2','3','q']:
-        if str(userChoice) == 'q':
-            sys.exit('Goodbye!')
-        else:
-            #AI conditionals
-            randChoice = str(random.randint(1, 3))
-            print (randChoice)
-            if randChoice == "1":
-                print("They chose Rock...")
-            if randChoice == "2":
-                print("They chose Paper...")
-            if randChoice == "3":
-                print("They chose Scissors...")
 
-            #user conditionals
+options = ['1', '2', '3', 'q']
+wins = 0
 
-            if userChoice == "1":
-                print("You chose Rock...")
-            if userChoice == "2":
-                print("You chose Paper...")
-            if userChoice == "3":
-                print("You chose Scissors...")
-
-            if str(userChoice) == randChoice:
-                print ("Tie")
-            elif userChoice == "1" and randChoice == "3" or userChoice == "2" and randChoice == "1" or userChoice =="3" and randChoice =="2":
-                print("You win!")
-            else:
-                print('You lose.')
+def process_player_turn():
+    choice = input("Enter 1 for Rock, 2 for Paper and 3 For Scissors. To quit, enter 'q'\n")
+    if choice.lower() in options:
+        if choice == '1':
+            print("You chose ROCK")
+            return 'rock'
+        elif choice == '2':
+            print("You chose PAPER")
+            return 'paper'
+        elif choice == '3':
+            print("You chose SCISSORS")
+            return "scissors"
+        elif choice == 'q':
+            sys.exit("Game Over!")
     else:
-        print('Invalid input.\n')
+        return "Invalid choice"
+
+
+def process_cpu_turn():
+    choice = str(random.randint(1,3))
+    if choice == '1':
+        print("The Computer chooses ROCK")
+        return 'rock'
+    elif choice == '2':
+        print("The Computer chooses PAPER")
+        return 'paper'
+    elif choice == '3':
+        print("The Computer chooses SCISSORS")
+        return "scissors"
+    else:
+        return "Invalid choice"
+
+
+def roshambo():
+    global wins
+    wins = 0
+    userChoice = process_player_turn()
+
+    if str(userChoice).lower() == 'q':
+        sys.exit("Game Over!\nYou have won {} games!".format(wins))
+    else:
+
+        computerChoice = process_cpu_turn()
+
+        if userChoice == computerChoice:
+            print("Tie!")
+        elif userChoice == 'rock' and computerChoice == 'scissors' or userChoice == 'scissors' and computerChoice == 'paper' or userChoice == 'paper' and computerChoice == 'rock':
+            print("You win!")
+            wins = wins + 1
+        else:
+            print("You lose!")
+            
+        return wins
+
+
+
+
+
+
+def main():
+    choice = 'y'
+
+    while choice.lower() == 'y':
+        roshambo()
+        choice = input("Play again? (y/n): ")
+        if choice.lower() == 'n':
+            print("Game Over!\nYou have won {} games!".format(wins))
+            break
+
+
+main()
